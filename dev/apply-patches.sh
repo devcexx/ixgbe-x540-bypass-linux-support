b51d4d68c5d1ca24bc3ce2f327133a3f9fb28725
Roberto Guillén <robertoguillen@me.com>
2024-06-25 01:07:21 +0200

    Made apply-patches.sh generate stable commit hashes

--- dev/apply-patches.sh
+++ dev/apply-patches.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 repo_base_path=$(git rev-parse --show-toplevel)
 linux_root="$repo_base_path/linux"
 patches_root="$repo_base_path/patches"
-(cd "$linux_root" && git am "$patches_root/"*.patch)
+(cd "$linux_root" && git am --committer-date-is-author-date "$patches_root/"*.patch)
