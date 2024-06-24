a37a02262c7ef69f7eaaf1d78a83dbd49719ecb0
Roberto Guillén <robertoguillen@me.com>
2024-06-25 00:59:50 +0200

    Removed patches numeration

--- dev/build-patches.sh
+++ dev/build-patches.sh
@@ -5,4 +5,4 @@ repo_base_path=$(git rev-parse --show-toplevel)
 base_commit=$(cat "$repo_base_path/dev/base_commit")
 linux_root="$repo_base_path/linux"
 patches_root="$repo_base_path/patches"
-(cd "$linux_root" && git format-patch "$base_commit" -o "$patches_root")
+(cd "$linux_root" && git format-patch "$base_commit" --no-numbered -o "$patches_root")
